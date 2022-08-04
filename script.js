0c98870d24712e68af0e7cd606a99d3d7c61e3f2
TandiweP <87207713+TandiweP@users.noreply.github.com>
2022-08-04 17:23:55 -0600

    bootstrap card

--- script.js
+++ script.js
@@ -21,42 +21,19 @@ const horse = new Animal (
     false,
 );
 
+console.log(Animal);
+
 const content = `
-    <div className = "container-fluid" >
-        <h1>Animals:</h1>
-        <div>
-            <h1>${cat.id}: ${cat.name}</h1>
+<div class="card text-white bg-primary mb-3" style="width: 18rem;">
+    <div class="card-body">
+        <h3 class="card-body">${cat.id}: ${cat.name}:</h3>
             <ol>
-                <li>Id: ${cat.id}</li>
-                <li>Name: ${cat.name}</li>
                 <li>Sound: ${cat.sound}</li>
                 <li>clicked: ${cat.clicked}</li>
-
             </ol>
-        </div>
-        <div>
-            <h1>${dog.id}: ${dog.name}</h1>
-            <ol>
-
-                <li>Id: ${dog.id}</li>
-                <li>Name: ${dog.name}</li>
-                <li>Sound: ${dog.sound}</li>
-                <li>clicked: ${dog.clicked}</li>
-
-            </ol>
-        </div>
-        <div>
-            <h1>${horse.id}: ${horse.name}</h1>
-            <ol>
-            
-                <li>Id: ${horse.id}</li>
-                <li>Name: ${horse.name}</li>
-                <li>Sound: ${horse.sound}</li>
-                <li>clicked: ${horse.clicked}</li>
-
-            </ol>
-        </div>
-    </div>
+        <button class="btn-btn-primary">Get Sound</button>
+    </div>   
+</div>
 `
 
 document.body.innerHTML = content;
